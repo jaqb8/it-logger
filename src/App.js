2c254a48d24b424449b82be7ea2906d5d1ef0bfd
jaqb8 <46377510+jaqb8@users.noreply.github.com>
2019-08-26 20:38:55 +0200

    add floating buttons & log modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect, Fragment } from "react";
-import SearchBar from "./components/layout/SearchBar";
-import Logs from "./components/logs/Logs";
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 
-import "materialize-css/dist/css/materialize.min.css";
-import M from "materialize-css/dist/js/materialize.min.js";
-import "./App.css";
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
 
 const App = () => {
   useEffect(() => {
@@ -15,7 +17,9 @@ const App = () => {
   return (
     <Fragment>
       <SearchBar />
-      <div className="container">
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
