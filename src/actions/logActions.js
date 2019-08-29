1e7f8dbb958be6afa111cf90eeada8bc8e52cf08
jaqb8 <46377510+jaqb8@users.noreply.github.com>
2019-08-29 21:26:17 +0200

    add search bar funcionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from './types';
 
 // export const getLogs = () => {
@@ -103,6 +104,25 @@ export const updateLog = log => async dispatch => {
 
     dispatch({
       type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.res.data
+    });
+  }
+};
+
+// Search server logs
+export const searchLogs = text => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
       payload: data
     });
   } catch (err) {
