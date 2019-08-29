3c579335075383d10955c18ac07f02e6f7326eb8
jaqb8 <46377510+jaqb8@users.noreply.github.com>
2019-08-29 20:45:13 +0200

    add update logs aciton

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_LOG
 } from './types';
 
 // export const getLogs = () => {
@@ -82,6 +85,47 @@ export const deleteLog = id => async dispatch => {
       payload: err.res.data
     });
   }
+};
+
+// Update Current Log on server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.res.data
+    });
+  }
+};
+
+// Set Current log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  };
+};
+
+// Clear Current log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  };
 };
 
 // Set Loading to true
