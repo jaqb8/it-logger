f4a8b92c9a29ed1e4bb1077ca921dcc34a61e287
jaqb8 <46377510+jaqb8@users.noreply.github.com>
2019-09-01 16:22:35 +0200

    add Tech Options Select component

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -3,6 +3,7 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
+import TechSelectOptions from '../../components/techs/TechSelectOptions';
 
 const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
@@ -58,9 +59,7 @@ const AddLogModal = ({ addLog }) => {
             <option value='' disabled>
               Select Technician
             </option>
-            <option value='John Doe'>John Doe</option>
-            <option value='Sam Smith'>Sam Smith</option>
-            <option value='Sara Williams'>Sara Williams</option>
+            <TechSelectOptions />
           </select>
         </div>
 
